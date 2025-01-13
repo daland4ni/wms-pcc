163397f813f9f4b5e2f756d2d035b48d667e4928
daland4ni <aeronroxas01@gmail.com>
2025-01-14 05:37:57 +0800

    added adopt css and form elements css

--- pages/adopt.php
+++ pages/adopt.php
@@ -5,67 +5,88 @@
 
 <head>
     <title>Adoption Form</title>
+    <link rel="stylesheet" href="../css/adopt.css">
+    <link rel="stylesheet" href="../css/form-elements.css">
+    <link rel="icon" href="../pics/logo.png" type="image/x-icon"
 </head>
 
-<body>
+<body class="moolah">
 
     <?php include "nav.php"; ?>
-    <form>
-        <h1>Personal Information</h1>
-        <input type="text" placeholder="Full Name"><br />
-        <input type="number" placeholder="Age"><br />
 
-        <h1>Household Information</h1>
+    <div class="form-container">
+        <h2>Pet Adoption Form</h2>
+        <form>
+            <div class="form-group">
+                <div class="radio-container-row">
+                    <label><input type="radio" name="honorific" value="male">Mr.</label>
+                    <label><input type="radio" name="honorific" value="female">Ms.</label>
+                    <label><input type="radio" name="honorific" value="other">Mx.</label>
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="fname">First Name</label>
+                <input type="text" id="fname" name="fname" placeholder="Enter your first name">
+            </div>
+            <div class="form-group">
+                <label for="lname">Last Name</label>
+                <input type="text" id="lname" name="lname" placeholder="Enter your last name">
+            </div>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email" placeholder="Enter your email">
+            </div>
+            <div class="form-group">
+                <label for="phonenum">Phone Number</label>
+                <input type="text" id="phonenum" name="phonenum" placeholder="Enter your mobile number">
+            </div>
+            <div class="form-group">
+                <p>Monthly Income</p>
+                <div class="radio-container-col">
+                    <label><input type="radio" name="monthly" value="1">Less than ₱5,000</label>
+                    <label><input type="radio" name="monthly" value="2">₱5,000 to ₱10,000</label>
+                    <label><input type="radio" name="monthly" value="3">₱10,001 to ₱30,000</label>
+                    <label><input type="radio" name="monthly" value="4">₱30,001 to ₱50,000</label>
+                    <label><input type="radio" name="monthly" value="5">₱50,001 to ₱75,000</label>
+                    <label><input type="radio" name="monthly" value="6">More than ₱75,000</label>
+                </div>
+            </div>
+            <div class="form-group">
+                <p>Do you have any other pets in your household?</p>
+                <div class="radio-container-row">
+                    <label><input type="radio" name="otherpets" value="yes">Yes</label>
+                    <label><input type="radio" name="otherpets" value="no">No</label>
+                </div>
+            </div>
 
-        <h4>Monthly Income</h4>
-        <input type="radio" id="mi-op1" name="MI">
-        <label for="mi-op1">< PHP 5,000</label><br>
-        <input type="radio" id="mi-op2" name="MI">
-        <label for="mi-op2">PHP 5,000 - PHP 10,000</label><br>
-        <input type="radio" id="mi-op3" name="MI">
-        <label for="mi-op3">PHP 10,001 - PHP 30,000</label><br>
-        <input type="radio" id="mi-op4" name="MI">
-        <label for="mi-op4">PHP 30,001 - PHP 50,000</label><br>
-        <input type="radio" id="mi-op5" name="MI">
-        <label for="mi-op5">PHP 50,001 - PHP 75,000</label><br>
-        <input type="radio" id="mi-op6" name="MI">
-        <label for="mi-op6">> PHP 75,000</label><br>
-
-        <h4>Do you any other pets in your household?</h4>
-        <input type="radio" id="otherpet-yes" name="otherpet">
-        <label for="otherpet-yes">Yes</label><br>
-        <input type="radio" id="otherpet-no" name="otherpet">
-        <label for="otherpet-no">No</label><br>
-
-        <h4>Do you or any member of your household have known allergies to pet fur or any other allergens commonly found in pet animals?</h4>
-        <input type="radio" id="allergens-yes" name="allergens">
-        <label for="allergens-yes">Yes</label><br>
-        <input type="radio" id="allergens-no" name="allergens">
-        <label for="allergens-no">No</label><br>
-
-        <h4>How would your work setup affect your caretaking for your possible new pet?</h4>
-        <input type="radio" id="setup-one" name="work-setup">
-        <label for="setup-one">Work from Home setup and can take care of pets</label><br>
-        <input type="radio" id="setup-two" name="work-setup">
-        <label for="setup-two">Work from Home setup, can't take care of pets, but have someone else to attend to them</label><br>
-        <input type="radio" id="setup-three" name="work-setup">
-        <label for="setup-three">On site setup but someone can attend to pets</label><br>
-        <input type="radio" id="setup-four" name="work-setup">
-        <label for="setup-four">On site setup with no one to attend to pets</label><br>
-
-        <h4>A processing fee may be imposed by the associated shelter</h4>
-        <input type="radio" id="fee-yes" name="fee">
-        <label for="fee-yes">Yes, I understand</label><br>
-        <input type="radio" id="fee-no" name="fee">
-        <label for="fee-no">Not this time</label><br>
-
-
-        <!--
-
-        Long-term commitment
-
-        -->
-    </form>
+            <div class="form-group">
+                <p>Does anyone in your household have any allergic reaction to animals?</p>
+                <div class="radio-container-row">
+                    <label><input type="radio" name="allergen" value="yes">Yes</label>
+                    <label><input type="radio" name="allergen" value="no">No</label>
+                </div>
+            </div>
+            <div class="form-group">
+                <p>How does your work setup affect your pet fostering?</p>
+                <div class="radio-container-col">
+                    <label><input type="radio" name="worksetup" value="1">Work from Home but can attend to pet</label>
+                    <label><input type="radio" name="worksetup" value="2">Work from Home but someone else can attend to pet</label>
+                    <label><input type="radio" name="worksetup" value="3">On-site work but someone else can attend to pet</label>
+                    <label><input type="radio" name="worksetup" value="4">On-site work with no one else to attend to pet</label>
+                    <label><input type="radio" name="worksetup" value="5">Unemployed</label>
+                </div>
+            </div>
+            <div class="form-group">
+                <p>What is your residential type?</p>
+                <div class="radio-container-col">
+                    <label><input type="radio" name="residential" value="1">House is owned</label>
+                    <label><input type="radio" name="residential" value="2">In rental with no Pet Restrictions</label>
+                    <label><input type="radio" name="residential" value="3">In rental with Pet Restrictions</label>
+                </div>
+            </div>
+            <button type="submit" class="submit-btn">Submit</button>
+        </form>
+    </div>
 </body>
 
 </html>
