a0c802097c7f4ba24f7f72a442434582bffbbb0d
mowchi <justinebalbido@gmail.com>
2025-01-14 00:02:31 +0800

    home.php edit, home.css added, petnme added

--- pages/home.php
+++ pages/home.php
@@ -6,11 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pet Connect - Caloocan</title>
     <link rel="icon" href="../pics/logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="../css/index.css">
+    <link rel="stylesheet" href="../css/home.css">
 </head>
 <body>
     <?php include "nav.php"; ?>
-
-    <div>
+<!------------------------------------------------------------------->
+    <div class="container-1">
         <div class="left">
             <img src="../pics/CatDogLogo.png">
         </div>
@@ -23,15 +25,77 @@
             <button class="bt2">Start Adopting</button>
         </div>
     </div>
-
-    <div>
+<!-------------------------------------------------------------------->
+    <div class="container-2">
         <h2>Featured Pets</h2>
         <div class="featured-container">
             <div>
                 <img src="../pics/Cats/C1.jpg">
             </div>
 
+            <div>
+                <img src="../pics/Dogs/D1.jpg">
+            </div>
+
+            <div>
+                <img src="../pics/Cats/C2.jpg">
+            </div>
+
+            <div>
+                <img src="../pics/Dogs/D2.jpg">
+            </div>
+
+            <div>
+                <img src="../pics/Cats/C3.jpg">
+            </div>
+
+            <div>
+                <img src="../pics/Cats/C4.jpg">
+            </div>
+
+            <div>
+                <img src="../pics/Dogs/D3.jpg">
+            </div>
+
+            <div>
+                <img src="../pics/Cats/C5.jpg">
+            </div>
+
         </div>
+
+    </div>
+<!------------------------------------------------------------->    
+    <div class="container-3">
+        <h1>Why Adopt?</h1>
+
+        <p>Adopting a pet provides a loving home to an animal in need, 
+        giving it a second chance at happiness and security. 
+        It helps reduce the number of homeless animals, supports shelters, and offers a lifelong companion. 
+        Adoption also contributes to a sense of fulfillment and responsibility, 
+        making a positive impact on both your life and the pet's.</p>
+
+        <img class="" src="../pics/catdog.png">
+    </div>
+<!---------------------------------------------------------->
+    <div class="container-4">
+        <h1>Testimonials</h1>
+
+        <div>
+            <img src="../pics/PetnMe/testimony1.jpg">
+            <p>"dajkdabahb"</p>
+        </div>
+
+        <div>
+            <img src="../pics/PetnMe/testimony2.jpg">
+            <p>"dajkdabahb"</p>
+        </div>
+
+        <div>
+            <img src="../pics/PetnMe/testimony3.jpg">
+            <p>"dajkdabahb"</p>
+        </div>
+
+        <h1>Adopt Now!</h1>
 
     </div>
     
--- pages/index.php
+++ pages/index.php
@@ -1,6 +1,7 @@
 <html>
     <head>
-        <title></title>
+        <title>Pet Connect - Caloocan</title>
+        <link rel="icon" href="../pics/logo.png" type="image/x-icon">
     </head>
     <body>
     </body>
