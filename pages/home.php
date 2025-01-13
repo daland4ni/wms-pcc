d2b1d0941e0ac39ddef54092da7456ba01e01350
mowchi <justinebalbido@gmail.com>
2025-01-14 05:48:21 +0800

    home page edit

--- pages/home.php
+++ pages/home.php
@@ -7,9 +7,9 @@
     <title>Pet Connect - Caloocan</title>
     <link rel="icon" href="../pics/logo.png" type="image/x-icon">
     <link rel="stylesheet" href="../css/index.css">
-    <link rel="stylesheet" href="../css/home.css">
+    <link rel="stylesheet" href=".././css/home.css">
 </head>
-<body>
+<body class="bgc">
     <?php include "nav.php"; ?>
 <!------------------------------------------------------------------->
     <div class="container-1">
@@ -19,7 +19,7 @@
 
         <div class="right">
             <h1 class="t1">Hi Pet Lover!</h1>
-            <h2 class="subt">Find your furry pet today</h2>
+            <h3 class="subt">Find your furry pet today</h3>
 
             <button class="bt1">Explore Pets</button>
             <button class="bt2">Start Adopting</button>
@@ -29,35 +29,35 @@
     <div class="container-2">
         <h2>Featured Pets</h2>
         <div class="featured-container">
-            <div>
+            <div data-name="Kuting">
                 <img src="../pics/Cats/C1.jpg">
             </div>
 
-            <div>
+            <div data-name="Katriona">
                 <img src="../pics/Dogs/D1.jpg">
             </div>
 
-            <div>
+            <div data-name="Nuggets">
                 <img src="../pics/Cats/C2.jpg">
             </div>
 
-            <div>
+            <div data-name="Pua">
                 <img src="../pics/Dogs/D2.jpg">
             </div>
 
-            <div>
+            <div data-name="Molly">
                 <img src="../pics/Cats/C3.jpg">
             </div>
 
-            <div>
+            <div data-name="Sonny O.">
                 <img src="../pics/Cats/C4.jpg">
             </div>
 
-            <div>
+            <div data-name="Uno">
                 <img src="../pics/Dogs/D3.jpg">
             </div>
 
-            <div>
+            <div data-name="Bubbles">
                 <img src="../pics/Cats/C5.jpg">
             </div>
 
@@ -66,16 +66,24 @@
     </div>
 <!------------------------------------------------------------->    
     <div class="container-3">
-        <h1>Why Adopt?</h1>
+        <div class="text">
+            <h1>Why Adopt?</h1>
+            <p>Adopting a pet provides a loving home to an animal in need, 
+            giving it a second chance at happiness and security. 
+            It helps reduce the number of homeless animals, supports shelters, and offers a lifelong companion. 
+            Adoption also contributes to a sense of fulfillment and responsibility, 
+            making a positive impact on both your life and the pet's.</p>
 
-        <p>Adopting a pet provides a loving home to an animal in need, 
-        giving it a second chance at happiness and security. 
-        It helps reduce the number of homeless animals, supports shelters, and offers a lifelong companion. 
-        Adoption also contributes to a sense of fulfillment and responsibility, 
-        making a positive impact on both your life and the pet's.</p>
+            <button href="adopt.php">Adopt Now!</button>
+        </div>
+        <img src="../pics/catdog.png" alt="Cat and Dog">
 
-        <img class="" src="../pics/catdog.png">
+    
+         
+        
+        
     </div>
+
 <!---------------------------------------------------------->
     <div class="container-4">
         <h1>Testimonials</h1>
