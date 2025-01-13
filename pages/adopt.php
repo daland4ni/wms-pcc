893fa9446f55328ca91554a15e8fca201e3c1645
daland4ni <aeronroxas01@gmail.com>
2025-01-13 19:16:22 +0800

    Added adopt page and transitioned to PHP

---
+++ pages/adopt.php
@@ -0,0 +1,49 @@
+<!-- AERON -->
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <title>Adoption Form</title>
+</head>
+
+<body>
+    <form>
+        <input type="text" placeholder="Full Name"><br />
+        <input type="number" placeholder="Age"><br />
+
+        <h4>Monthly Income</h4>
+        <input type="radio" id="mi-op1">
+        <label for="mi-op1">< PHP 5,000</label><br>
+        <input type="radio" id="mi-op2">
+        <label for="mi-op2">PHP 5,000 - PHP 10,000</label><br>
+        <input type="radio" id="mi-op3">
+        <label for="mi-op3">PHP 10,001 - PHP 30,000</label><br>
+        <input type="radio" id="mi-op4">
+        <label for="mi-op4">PHP 30,001 - PHP 50,000</label><br>
+        <input type="radio" id="mi-op5">
+        <label for="mi-op5">PHP 50,001 - PHP 75,000</label><br>
+        <input type="radio" id="mi-op6">
+        <label for="mi-op6">> PHP 75,000</label><br>
+
+        
+
+        <!--
+        
+        Name
+        Age
+        Monthly Income
+        Time Commitment
+        Allergens
+        Experience in Pet Caring
+        Inspections
+        Processing Fee
+        Other pet
+        Long-term commitment
+
+
+        -->
+    </form>
+</body>
+
+</html>
