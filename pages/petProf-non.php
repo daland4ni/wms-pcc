26f7eec9252b7f75c5c81ebf450d8ce069ddc93e
Aeron Roxas <aeronroxas01@gmail.com>
2025-02-05 12:08:13 +0800

    updated pet profiles

---
+++ pages/petProf-non.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Image Gallery</title>
+  <link rel="stylesheet" href="../css/petProf.css">
+</head>
+
+<body>
+
+  <?php include "nav.php"; ?>
+
+  <div class="content">
+    <div class="gallery-name">
+      <p>Pet Profiles</p>
+    </div>
+    <div class="gallery">
+      <div class="gallery-item">
+        <a href="#popup1"><img src="../pics/Cats/C1.jpg" alt="Pet 1"></a>
+        
+        <div class="caption"><b>Andrew</b>, 1 year old, Male, Tabi Cat</div>
+      </div>
+      <div class="gallery-item">
+        <img src="../pics/Cats/C2.jpg" alt="Pet 2">
+        <div class="caption"><b>Aivan</b>, 2 years old, Male, Ginger Tabi</div>
+      </div>
+      <div class="gallery-item">
+        <img src="../pics/Dogs/D1.jpg" alt="Pet 3">
+        <div class="caption"><b>Julius</b>, 3 months, Male, Cross Breed</div>
+      </div>
+      <div class="gallery-item">
+        <img src="../pics/Cats/C3.jpg" alt="Pet 4">
+        <div class="caption"><b>Trina</b>, 3 years old, Female, Cross Breed</div>
+      </div>
+      <div class="gallery-item">
+        <img src="../pics/Dogs/D2.jpg" alt="Pet 5">
+        <div class="caption"><b>Daniel</b>, 3 months, Male, Cross Breed</div>
+      </div>
+      <div class="gallery-item">
+        <img src="../pics/Dogs/D3.jpg" alt="Pet 6">
+        <div class="caption"><b>Shein</b>, 1 year old, Female, Aspin</div>
+      </div>
+
+      <div class="gallery-item">
+        <img src="../pics/Cats/C4.jpg" alt="Pet 4">
+        <div class="caption"><b>Marvic</b>, 1 year old, Male, Ginger Tabi</div>
+      </div>
+      <div class="gallery-item">
+        <img src="../pics/Dogs/D4.png" alt="Pet 5">
+        <div class="caption"><b>Rowine</b>, 7 months, male, Shit tzu</div>
+      </div>
+      <div class="gallery-item">
+        <img src="../pics/Dogs/D5.png" alt="Pet 6">
+        <div class="caption"><b>Edda</b>, 5 months, Female, Canaan</div>
+      </div>
+      <div class="gallery-item">
+        <img src="../pics/Cats/C5.jpg" alt="Pet 4">
+        <div class="caption"><b>Trisha</b>, 2 years old, Female, Siamese</div>
+      </div>
+      <div class="gallery-item">
+        <img src="../pics/Dogs/D6.png" alt="Pet 5">
+        <div class="caption"><b>Tom</b>, 4 years old, Male, Aspin</div>
+      </div>
+      <div class="gallery-item">
+        <img src="../pics/Cats/Csquare.png" alt="Pet 6">
+        <div class="caption"><b>Mark</b>, 4 years old, Male, Cross Breed</div>
+      </div>
+
+      <div class="gallery-item">
+        <img src="../pics/Cats/C7.png" alt="Pet 4">
+        <div class="caption"><b>James</b>, 8 months, Male, Cross Breed</div>
+      </div>
+      <div class="gallery-item">
+        <img src="../pics/Dogs/D7.png" alt="Pet 5">
+        <div class="caption"><b>Trishia</b>, 1 year old, Female, Aspin</div>
+      </div>
+      <div class="gallery-item">
+        <img src="../pics/Dogs/D8.png" alt="Pet 6">
+        <div class="caption"><b>Angelo</b>, 2 years old, Male, Crossbreed</div>
+      </div>
+      <div class="gallery-item">
+        <img src="../pics/Cats/C8.png" alt="Pet 4">
+        <div class="caption"><b>Jamaica</b>, 3 years old, Female, Cross Breed</div>
+      </div>
+      <div class="gallery-item">
+        <img src="../pics/Dogs/D9.png" alt="Pet 5">
+        <div class="caption"><b>Jane</b>, 3 months, Female, Jack Russel</div>
+      </div>
+      <div class="gallery-item">
+        <img src="../pics/Cats/C9.png" alt="Pet 6">
+        <div class="caption"><b>Joy</b>, 2 months, Female, British Shorthair</div>
+      </div>
+
+      <div class="gallery-item">
+        <img src="../pics/Cats/C10.png" alt="Pet 4">
+        <div class="caption"><b>Rico</b>, 3 months, Male, LaPerm</div>
+      </div>
+      <div class="gallery-item">
+        <img src="../pics/Dogs/D10.png" alt="Pet 5">
+        <div class="caption"><b>Karl</b>, 3 years old, Male, Aspin</div>
+      </div>
+      <div class="gallery-item">
+        <img src="../pics/Dogs/D11.png" alt="Pet 6">
+        <div class="caption"><b>Migs</b>, 1 year old, Male, Pitbul</div>
+      </div>
+      <div class="gallery-item">
+        <img src="../pics/Cats/C11.png" alt="Pet 4">
+        <div class="caption"><b>Dwayne</b>, 3 years old, Male, Cross Breed</div>
+      </div>
+      <div class="gallery-item">
+        <img src="../pics/Dogs/D12.png" alt="Pet 5">
+        <div class="caption"><b>Anthony</b>, 5 years old, Male, Aspin</div>
+      </div>
+      <div class="gallery-item">
+        <img src="../pics/Cats/C12.png" alt="Pet 6">
+        <div class="caption"><b>Vincent</b>, 4 years, Male, Ginger Tabi</div>
+      </div>
+
+      <div class="gallery-item">
+        <img src="../pics/Cats/C13.png" alt="Pet 4">
+        <div class="caption"><b>Neil</b>, 4 months, Male, Cross Breed</div>
+      </div>
+      <div class="gallery-item">
+        <img src="../pics/Dogs/D13.png" alt="Pet 5">
+        <div class="caption"><b>Trixia</b>, 4 months, Female, Chinook</div>
+      </div>
+      <div class="gallery-item">
+        <img src="../pics/Dogs/D14.png" alt="Pet 6">
+        <div class="caption"><b>Rei</b>, 3 years old, Male, Dutch hound</div>
+      </div>
+      <div class="gallery-item">
+        <img src="../pics/Cats/C14square.png" alt="Pet 4">
+        <div class="caption"><b>Shane</b>, 4 years old, Female, Bengal Cat</div>
+      </div>
+      <div class="gallery-item">
+        <img src="../pics/Dogs/D15.png" alt="Pet 5">
+        <div class="caption"><b>Evan</b>, 1 year old, Male, Poodle</div>
+      </div>
+      <div class="gallery-item">
+        <img src="../pics/Cats/C15.png" alt="Pet 6">
+        <div class="caption"><b>Malone</b>, 1 year and 4 months, Male, Cross Breed</div>
+      </div>
+
+      <div class="gallery-item">
+        <img src="../pics/Cats/C16.png" alt="Pet 4">
+        <div class="caption"><b>Carem</b>, 4 months, Female, Tabi</div>
+      </div>
+      <div class="gallery-item">
+        <img src="../pics/Dogs/D16.png" alt="Pet 5">
+        <div class="caption"><b>Ethan</b>, 4 years old, Male, Chihuahua</div>
+      </div>
+      <div class="gallery-item">
+        <img src="../pics/Dogs/D17.png" alt="Pet 6">
+        <div class="caption"><b>Joana</b>, 1 month, female, Jack Russel</div>
+      </div>
+      <div class="gallery-item">
+        <img src="../pics/Cats/C17.png" alt="Pet 4">
+        <div class="caption"><b>Ranzi</b>, 8 months, Male, Ginger Tabi</div>
+      </div>
+      <div class="gallery-item">
+        <img src="../pics/Dogs/D18.png" alt="Pet 5">
+        <div class="caption"><b>Khyle</b>, 1 month female, Golden Retriever</div>
+      </div>
+      <div class="gallery-item">
+        <img src="../pics/Cats/C18.png" alt="Pet 6">
+        <div class="caption"><b>Clark</b>, 3 years, Male, Tabi</div>
+      </div>
+
+      <div class="gallery-item">
+        <img src="../pics/Cats/C19.png" alt="Pet 4">
+        <div class="caption"><b>Idriz</b>, 4 years, Male, Cross Breed</div>
+      </div>
+      <div class="gallery-item">
+        <img src="../pics/Dogs/D19.png" alt="Pet 5">
+        <div class="caption"><b>Joaquin</b>, 1 year, Male, Shit tzu</div>
+      </div>
+      <div class="gallery-item">
+        <img src="../pics/Dogs/D20.png" alt="Pet 6">
+        <div class="caption"><b>Diana</b>, 2 years old, Female, Aspin</div>
+      </div>
+      <div class="gallery-item">
+        <img src="../pics/Cats/C20.png" alt="Pet 4">
+        <div class="caption"><b>Ernest</b>, 3 years, Male, Cross Breed</div>
+      </div>
+
+      <div id="popup1" class="popup">
+        <div class="popup-content">
+            <img src="../pics/Cats/C1.jpg" alt="Pet 1">
+            <p>This is a detailed description of Pet 1.</p>
+            <a href="#" class="close">Close</a>
+        </div>
+    </div>
+
+    </div>
+  </div>
+
+  </div>
+</body>
+
+</html>
