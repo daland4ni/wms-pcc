55b6afe427a022d377c3cb88ba9d94661bc87575
Aeron Roxas <aeronroxas01@gmail.com>
2025-02-05 21:34:38 +0800

    initialized match-a-pet php and css / edited nav hrefs

---
+++ pages/matching.php
@@ -0,0 +1,24 @@
+<!-- AERON -->
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <title>Match-A-Pet</title>
+    <link rel="stylesheet" href="../css/matching.css">
+    <link rel="stylesheet" href="../css/adopt.css">
+    <link rel="stylesheet" href="../css/form-elements.css">
+    <link rel="icon" href="../pics/logo.png" type="image/x-icon"
+</head>
+
+<body class="moolah">
+
+    <?php include "nav.php"; ?>
+
+    <div class="uno">
+        .
+    </div>
+
+
+
+</body>
--- pages/nav.php
+++ pages/nav.php
@@ -21,9 +21,9 @@
         <div class="nav-right">
             <a href="home.php">Home</a> <!-- justine -->
             <a href="petProf.php">Pet Profiles</a> <!-- adelaine -->
-            <a href="#match">Match-A-Pet</a> <!-- aeron -->
+            <a href="matching.php">Match-A-Pet</a> <!-- aeron -->
             <a href="adopt.php">Adopt Now!</a> <!-- aeron -->
-            <a href="#shelter">Meet the Rehomers</a> <!-- rence -->
+            <a href="fosters.php">Meet the Rehomers</a> <!-- rence -->
         </div>
     </nav>
     <main>
