250f9d1cd7959b3f1e34ace93e99a80c1528aa8d
Gray2912 <lorence.figueroa2000@gmail.com>
2025-02-07 17:00:35 +0800

    added pet name on rehomers

--- pages/fosters.php
+++ pages/fosters.php
@@ -26,121 +26,123 @@
     <div class="gallery">
       <div class="gallery-item">
         <img src="../pics/fosters/balatico.png" alt="Pet 1">
-        <div class="caption"><b>Balatico</b> <br> Contact Info:</br> <br>balaticorei@gmail.com </br>
+        <div class="caption"><b>Balatico</b> <br> Contact Info:</br> <br>balaticorei@gmail.com </br><br>Pet Name: 
+        Dog, Rei</br>
         </div>
       </div>
       <div class="gallery-item">
         <img src="../pics/fosters/caballes.png" alt="Pet 2">
-        <div class="caption"><b>Caballes</b><br>Contact Info:</br><br>dgavcwork@gmail.com</br></div>
+        <div class="caption"><b>Caballes</b><br>Contact Info:</br><br>dgvcwork@gmail.com</br><br>Pet Name: Dog, Daniel</br>
+        </div>
       </div>
       <div class="gallery-item">
         <img src="../pics/fosters/bermudez.png" alt="Pet 3">
-        <div class="caption"><b>Bermudez</b><br>Contact Info:</br><br>bermudez@gmail.com</br></div>
+        <div class="caption"><b>Bermudez</b><br>Contact Info:</br><br>bermudez@gmail.com</br><br>Pet Name: Cat, Trisha</br></div>
       </div>
       <div class="gallery-item">
         <img src="../pics/fosters/cabanban.png" alt="Pet 4">
-        <div class="caption"><b>Cabanban</b><br>Contact Info:</br><br>cabanban@gmail.com</br></div>
+        <div class="caption"><b>Cabanban</b><br>Contact Info:</br><br>cabanban@gmail.com</br><br>Pet Name: Dog, Jane</br></div>
       </div>
       <div class="gallery-item">
         <img src="../pics/fosters/deguzman.png" alt="Pet 5">
-        <div class="caption"><b>De Guzman</b><br>Contact Info:</br><br>aivandg@gmail.com</br></div>
+        <div class="caption"><b>De Guzman</b><br>Contact Info:</br><br>aivandg@gmail.com</br><br>Pet Name: Cat, Aivan</br></div>
       </div>
       <div class="gallery-item">
         <img src="../pics/fosters/delcastillo.png" alt="Pet 6">
-        <div class="caption"><b>Del Castillo</b><br>Contact Info:</br><br>tpogingz@gmail.com</br></div>
+        <div class="caption"><b>Del Castillo</b><br>Contact Info:</br><br>tpogingz@gmail.com</br><br>Pet Name: Cat, Rico</br></div>
       </div>
 
       <div class="gallery-item">
         <img src="../pics/fosters/diane.png" alt="Pet 4">
-        <div class="caption"><b>Diane</b><br>Contact Info:</br><br>jdiane@gmail.com</br></div>
+        <div class="caption"><b>Diane</b><br>Contact Info:</br><br>jdiane@gmail.com</br><br>Pet Name: Dog, Migs</br></div>
       </div>
       <div class="gallery-item">
         <img src="../pics/fosters/francisco.png" alt="Pet 5">
-        <div class="caption"><b>Francisco</b><br>Contact Info:</br><br>julius43@gmail.com</br></div>
+        <div class="caption"><b>Francisco</b><br>Contact Info:</br><br>julius43@gmail.com</br><br>Pet Name: Dog, Julius</br></div>
       </div>
       <div class="gallery-item">
         <img src="../pics/fosters/fulgencio.png" alt="Pet 6">
-        <div class="caption"><b>Fulgencio</b><br>Contact Info:</br><br>trix.f@gmail.com</br></div>
+        <div class="caption"><b>Fulgencio</b><br>Contact Info:</br><br>trix.f@gmail.com</br><br>Pet Name: Dog, Trixia</br></div>
       </div>
       <div class="gallery-item">
         <img src="../pics/fosters/jose.png" alt="Pet 4">
-        <div class="caption"><b>Jose</b><br>Contact Info:</br><br>migzz0910@gmail.com</br></div>
+        <div class="caption"><b>Jose</b><br>Contact Info:</br><br>migzz0910@gmail.com</br><br>Pet Name: Dog, Tom</br></div>
       </div>
       <div class="gallery-item">
         <img src="../pics/fosters/mabiog.png" alt="Pet 4">
-        <div class="caption"><b>Mabiog</b><br>Contact Info:</br><br>mabiog24@gmail.com</br></div>
+        <div class="caption"><b>Mabiog</b><br>Contact Info:</br><br>mabiog24@gmail.com</br><br>Pet Name: Dog, Daniel</br></div>
       </div>
       <div class="gallery-item">
         <img src="../pics/fosters/mejia.png" alt="Pet 4">
-        <div class="caption"><b>Mejia</b><br>Contact Info:</br><br>sai@gmail.com</br></div>
+        <div class="caption"><b>Mejia</b><br>Contact Info:</br><br>sai@gmail.com</br><br>Pet Name: Dog, Trishia</br></div>
       </div>
       <div class="gallery-item">
         <img src="../pics/fosters/mercado.png" alt="Pet 4">
-        <div class="caption"><b>Mercado</b><br>Contact Info:</br><br>smercado@gmail.com</br></div>
+        <div class="caption"><b>Mercado</b><br>Contact Info:</br><br>smercado@gmail.com</br><br>Pet Name: Cat, Shane</br></div>
       </div>
       <div class="gallery-item">
         <img src="../pics/fosters/mitiam.png" alt="Pet 4">
-        <div class="caption"><b>Mitiam</b><br>Contact Info:</br><br>idriz@gmail.com</br></div>
+        <div class="caption"><b>Mitiam</b><br>Contact Info:</br><br>idriz@gmail.com</br><br>Pet Name: Dog, Joaquin</br></div>
       </div>
       <div class="gallery-item">
         <img src="../pics/fosters/mones.png" alt="Pet 4">
-        <div class="caption"><b>Mones</b><br>Contact Info:</br><br>ceejay@gmail.com</br></div>
+        <div class="caption"><b>Mones</b><br>Contact Info:</br><br>ceejay@gmail.com</br><br>Pet Name: Cat, Clark</br></div>
       </div>
       <div class="gallery-item">
         <img src="../pics/fosters/nabus.png" alt="Pet 4">
-        <div class="caption"><b>Nabus</b><br>Contact Info:</br><br>jmnabus@gmail.com</br></div>
+        <div class="caption"><b>Nabus</b><br>Contact Info:</br><br>jmnabus@gmail.com</br><br>Pet Name: Cat, Marvic</br></div>
       </div>
       <div class="gallery-item">
         <img src="../pics/fosters/nebre.png" alt="Pet 4">
-        <div class="caption"><b>Nebre</b><br>Contact Info:</br><br>jpnebre@gmail.com</br></div>
+        <div class="caption"><b>Nebre</b><br>Contact Info:</br><br>jpnebre@gmail.com</br><br>Pet Name: Cat, Jamaica</br></div>
       </div>
       <div class="gallery-item">
         <img src="../pics/fosters/pabiona.png" alt="Pet 4">
-        <div class="caption"><b>Pabiona</b><br>Contact Info:</br><br>pabiona@gmail.com</br></div>
+        <div class="caption"><b>Pabiona</b><br>Contact Info:</br><br>pabiona@gmail.com</br><br>Pet Name: Dog, Ethan</br></div>
       </div>
       <div class="gallery-item">
         <img src="../pics/fosters/padullo.png" alt="Pet 4">
-        <div class="caption"><b>Padullo</b><br>Contact Info:</br><br>padullod@gmail.com</br></div>
+        <div class="caption"><b>Padullo</b><br>Contact Info:</br><br>padullod@gmail.com</br><br>Pet Name: Dog, Khyle</br></div>
       </div>
       <div class="gallery-item">
         <img src="../pics/fosters/peralta.png" alt="Pet 4">
-        <div class="caption"><b>Peralta</b><br>Contact Info:</br><br>jrperalta@gmail.com</br></div>
+        <div class="caption"><b>Peralta</b><br>Contact Info:</br><br>jrperalta@gmail.com</br><br>Pet Name: Dog, Angelo</br></div>
       </div>
       <div class="gallery-item">
         <img src="../pics/fosters/plamenco.png" alt="Pet 4">
-        <div class="caption"><b>Plamenco</b><br>Contact Info:</br><br>angelo@gmail.com</br></div>
+        <div class="caption"><b>Plamenco</b><br>Contact Info:</br><br>angelo@gmail.com</br><br>Pet Name: Cat, Trina</br></div>
       </div>
       <div class="gallery-item">
         <img src="../pics/fosters/quitong.png" alt="Pet 4">
-        <div class="caption"><b>Quitong</b><br>Contact Info:</br><br>mayca@gmail.com</br></div>
+        <div class="caption"><b>Quitong</b><br>Contact Info:</br><br>mayca@gmail.com</br><br>Pet Name: Dog, Shein</br></div>
       </div>
       <div class="gallery-item">
         <img src="../pics/fosters/reyes.png" alt="Pet 4">
-        <div class="caption"><b>Reyes</b><br>Contact Info:</br><br>neil@gmail.com</br></div>
+        <div class="caption"><b>Reyes</b><br>Contact Info:</br><br>neil@gmail.com</br><br>Pet Name: Cat, Andrew</br></div>
       </div>
       <div class="gallery-item">
         <img src="../pics/fosters/rivera.png" alt="Pet 4">
-        <div class="caption"><b>Rivera</b><br>Contact Info:</br><br>rivera@gmail.com</br></div>
+        <div class="caption"><b>Rivera</b><br>Contact Info:</br><br>rivera@gmail.com</br><br>Pet Name: Cat, Joy</br></div>
       </div>
       <div class="gallery-item">
         <img src="../pics/fosters/sagun.png" alt="Pet 4">
-        <div class="caption"><b>Sagun</b><br>Contact Info:</br><br>vincent@gmail.com</br></div>
+        <div class="caption"><b>Sagun</b><br>Contact Info:</br><br>vincent@gmail.com</br><br>Pet Name: Cat, Vincent</br></div>
       </div>
       <div class="gallery-item">
         <img src="../pics/fosters/serrano.png" alt="Pet 4">
-        <div class="caption"><b>Serrano</b><br>Contact Info:</br><br>james028@gmail.com</br></div>
+        <div class="caption"><b>Serrano</b><br>Contact Info:</br><br>james028@gmail.com</br><br>Pet Name: Cat, James</br></div>
       </div>
       <div class="gallery-item">
         <img src="../pics/fosters/tadeo.png" alt="Pet 4">
-        <div class="caption"><b>Tadeo</b><br>Contact Info:</br><br>erneest@gmail.com</br></div>
+        <div class="caption"><b>Tadeo</b><br>Contact Info:</br><br>erneest@gmail.com</br><br>Pet Name: Cat, Ernest</br></div>
       </div>
       <div class="gallery-item">
         <img src="../pics/fosters/umali.png" alt="Pet 4">
-        <div class="caption"><b>Umali</b><br>Contact Info:</br><br>umali29409gmail.com</br></div>
+        <div class="caption"><b>Umali</b><br>Contact Info:</br><br>umali29409gmail.com</br><br>Pet Name: Dog, Evan</br></div>
       </div>
       <div class="gallery-item">
         <img src="../pics/fosters/villaruz.png" alt="Pet 4">
-        <div class="caption"><b>Villaruz</b><br>Contact Info:</br><br>ranzi@gmail.com</br></div>
+        <div class="caption"><b>Villaruz</b><br>Contact Info:</br><br>ranzi@gmail.com</br><br>Pet Name: Cat, Ranzi</br></div>
       </div>
     </div>
     <?php include "footer.php"; ?>
